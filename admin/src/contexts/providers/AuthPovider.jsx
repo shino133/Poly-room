a040d2ac730fa076600cc9c5245c18470458c916
Shino303 <locshino123@gmail.com>
2024-07-24 14:13:36 +0700

    Add Context Provider

--- admin/src/contexts/providers/AuthPovider.jsx
+++ admin/src/contexts/providers/AuthPovider.jsx
@@ -7,22 +7,13 @@ export const AuthProvider = ({ children }) => {
   const [userToken, _setUserToken] = useState(
     localStorage.getItem("TOKEN") || ""
   );
-  const [userRole, setUserRole] = useState("client");
-  const [questionTypes] = useState([
-    "text",
-    "select",
-    "radio",
-    "checkbox",
-    "textarea",
-  ]);
+  const [userRole, setUserRole] = useState(import.meta.env.VITE_API_ROLE || "");
   const [toast, setToast] = useState({ message: "", show: false });
 
   const setUserToken = (token) => {
-    if (token) {
-      localStorage.setItem("TOKEN", token);
-    } else {
-      localStorage.removeItem("TOKEN");
-    }
+    token //WHEN: token is already exits
+      ? localStorage.setItem("TOKEN", token)
+      : localStorage.removeItem("TOKEN");
     _setUserToken(token);
   };
 
@@ -39,7 +30,6 @@ export const AuthProvider = ({ children }) => {
         currentUser,
         userToken,
         userRole,
-        questionTypes,
         toast,
         showToast,
         setCurrentUser,
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { AuthProvider } from './contexts/providers'
+import { RouterProvider } from 'react-router-dom'
+import router from './Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
