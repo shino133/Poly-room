92a6fcb43b09167fe1471b2fb8ecd025779bd4de
quynhtranhuu <quynhthpn00114@gmail.com>
2024-07-26 16:39:56 +0700

    update_FormBooking_Admin

--- admin/src/views/pages/Booking.jsx
+++ admin/src/views/pages/Booking.jsx
@@ -5,15 +5,28 @@ export default function Booking() {
   const [userCode, setUserCode] = useState("");
   const [startTime, setStartTime] = useState("");
   const [endTime, setEndTime] = useState("");
+  const [date, setDate] = useState("");
+  const [note, setNote] = useState(""); // Thêm trạng thái cho ghi chú
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Xử lý gửi biểu mẫu ở đây
+    console.log("Room Code:", roomCode);
+    console.log("User Code:", userCode);
+    console.log("Start Time:", startTime);
+    console.log("End Time:", endTime);
+    console.log("Date:", date);
+    console.log("Note:", note);
+  };
 
   return (
     <>
-      <div className="w-4/5 mx-auto border-2 border-gray-300 p-4 rounded-md mt-10">
+      <div className="w-4/5 mx-auto  p-4 rounded-md mt-10">
         <h1 className="text-center font-sans font-semibold text-3xl pt-3 pb-3">
           Đặt phòng
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="roomCode" className="block font-medium">
               Mã phòng:
@@ -21,49 +34,58 @@ export default function Booking() {
             <input
               type="text"
               id="roomCode"
+              name="roomCode"
               value={roomCode}
               onChange={(e) => setRoomCode(e.target.value)}
               placeholder="Vui lòng nhập mã"
               className="rounded border-gray-300 border p-2 w-full"
               required
-              //   disabled
             />
           </div>
 
-          <div className="mb-4">
-            <label htmlFor="startTime" className="block font-medium">
-              Bắt đầu:
+          <div className="mb-4 flex gap-4">
+            <div className="flex-1">
+              <label htmlFor="startTime" className="block font-medium">
+                Bắt đầu:
+              </label>
+              <input
+                type="time"
+                id="startTime"
+                name="startTime"
+                value={startTime}
+                onChange={(e) => setStartTime(e.target.value)}
+                className="rounded border-gray-300 border p-2 w-full"
+                required
+              />
+            </div>
+
+            <div className="flex-1">
+              <label htmlFor="endTime" className="block font-medium">
+                Kết thúc:
+              </label>
+              <input
+                type="time"
+                id="endTime"
+                name="endTime"
+                value={endTime}
+                onChange={(e) => setEndTime(e.target.value)}
+                className="rounded border-gray-300 border p-2 w-full"
+                required
+              />
+            </div>
+          </div>
+          <div className="">
+            <label htmlFor="note" className=" font-medium">
+              Ghi chú:
             </label>
-            <input
-              type="time"
-              id="startTime"
-              value={startTime}
-              onChange={(e) => setStartTime(e.target.value)}
+            <textarea
+              id="note"
+              name="note"
+              value={note}
+              onChange={(e) => setNote(e.target.value)}
               className="rounded border-gray-300 border p-2 w-full"
-              required
             />
           </div>
-
-          <div className="mb-4">
-            <label htmlFor="endTime" className="block font-medium">
-              Kết thúc:
-            </label>
-            <input
-              type="time"
-              id="endTime"
-              value={endTime}
-              onChange={(e) => setEndTime(e.target.value)}
-              className="rounded border-gray-300 border p-2 w-full"
-              required
-            />
-            <label
-              htmlFor="date"
-              className="rounded border-gray-300 border p-2 w-full"
-            >
-              Ngày:
-            </label>
-          </div>``
-
           <button
             type="submit"
             className="px-4 py-2 bg-blue-500 text-white rounded"
