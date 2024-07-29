57961b967a755a27e903c50c5552fffbc7c611d4
RestiaGnoth <thongpmpn00089@gmail.com>
2024-07-29 16:34:52 +0700

    sửa logic update user của admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call(RoomChildSeeder::class);
 
+        $this->call(UpdateUserRoleAndStatusSeeder::class);
+
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
