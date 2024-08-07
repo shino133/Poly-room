3aa91aecd851c58ce20340cb7da4ef7cce105218
quynhtranhuu <quynhthpn00114@gmail.com>
2024-08-07 15:51:33 +0700

    booking_client

--- client/src/views/pages/Booking.jsx
+++ client/src/views/pages/Booking.jsx
@@ -1,17 +1,10 @@
 import React, { useState, useEffect } from "react";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
 import { getRoomData, bookRoom } from "../../Api";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import FormControl from "@mui/material/FormControl";
-import Button from "@mui/material/Button";
 import dayjs from "dayjs";
 import Snackbar from "@mui/material/Snackbar";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
+import FormBooking from "../../components/FormBooking";
 
 export default function Booking() {
   const [roomCode, setRoomCode] = useState("");
@@ -66,6 +59,13 @@ export default function Booking() {
     setEndTime(newValue ? dayjs(newValue).format("YYYY-MM-DDTHH:mm") : null);
   };
 
+  const handleNoteChange = (e) => {
+    setNote(e.target.value);
+  };
+  const handleRoomIdChange = (event, newValue) => {
+    setRoomCode(newValue);
+  };
+
   const handleBookRoom = () => {
     // create formdata
     const formData = new FormData();
@@ -107,69 +107,22 @@ export default function Booking() {
 
   return (
     <>
-      <div className="max-w-[500px] mx-auto rounded-tl-md rounded-tr-md p-4 mt-24 border-2 border-black-600">
+      <div className="min-w-[800px] mx-auto rounded-lg p-4 mt-24 border-2 border-white-600 bg-white">
         <h1 className="text-center font-bold text-blue-950 text-3xl m-4">
           Đặt phòng
         </h1>
-
-        <form class="max-w-[500px] mx-auto">
-          <div className="max-w-[500px] mx-auto mt-10">
-            <Autocomplete
-              disablePortal
-              options={options}
-              getOptionKey={(option) => option.key}
-              fullWidth={true}
-              value={roomCode}
-              onChange={(event, newValue) => {
-                setRoomCode(newValue);
-              }}
-              renderInput={(params) => (
-                <TextField {...params} label="Mã phòng" />
-              )}
-              noOptionsText="Đang tải dữ liệu..."
-            />
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoContainer components={["DateTimePicker"]}>
-                <DateTimePicker
-                  label="Giờ bắt đầu"
-                  value={startTime ? dayjs(startTime) : null}
-                  onChange={handleStartTimeChange}
-                  format="DD/MM/YYYY HH:mm"
-                />
-              </DemoContainer>
-            </LocalizationProvider>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoContainer components={["DateTimePicker"]}>
-                <DateTimePicker
-                  label="Giờ kết thúc"
-                  value={endTime ? dayjs(endTime) : null}
-                  onChange={handleEndTimeChange}
-                  format="DD/MM/YYYY HH:mm"
-                />
-              </DemoContainer>
-            </LocalizationProvider>
-            <FormControl fullWidth>
-              <TextField
-                label="Ghi chú"
-                value={note}
-                onChange={(e) => setNote(e.target.value)}
-                style={{ marginTop: 8 }}
-                variant="outlined"
-              />
-            </FormControl>
-            <Button
-              variant="contained"
-              style={{
-                backgroundColor: "#504b8e",
-                marginTop: "1rem",
-                marginBottom: "1.5rem",
-              }}
-              onClick={handleBookRoom}
-            >
-              Đặt phòng
-            </Button>
-          </div>
-        </form>
+        <FormBooking
+          options={options}
+          roomCode={roomCode}
+          startTime={startTime}
+          endTime={endTime}
+          note={note}
+          handleStartTimeChange={handleStartTimeChange}
+          handleEndTimeChange={handleEndTimeChange}
+          handleBookRoom={handleBookRoom}
+          handleNoteChange={handleNoteChange}
+          handleRoomIdChange={handleRoomIdChange}
+        />
         <Snackbar
           open={snackOpen}
           autoHideDuration={6000}
